40c79ab61dd883150f194e0f15a52a8cd92839b5
Elliott Minns <elliott.minns@me.com>
2016-02-03 17:01:07 +0000

    Added in saving to temp directory and path for Multipart files

--- Sources/MultipartFile.swift
+++ Sources/MultipartFile.swift
@@ -6,10 +6,42 @@
 //  Copyright © 2016 Elliott Minns. All rights reserved.
 //
 
+import Foundation
+#if os(Linux)
+    import Glibc
+#endif
+
+enum FileError: ErrorType {
+    case WriteError
+}
+
 struct MultipartFile {
     
     let name: String
     
     let data: [UInt8]
+    
+    func saveToTemporaryDirectory(filename: String, completion: ((path: String?, error: ErrorType?) -> Void)) {
+        let path = NSTemporaryDirectory() + "\(filename)"
+        saveToPath(path, completion: completion)
+    }
+    
+    func saveToPath(path: String, completion: ((path: String?, error: ErrorType?) -> Void)) {
+    
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+            
+            let raw = NSData(bytesNoCopy: UnsafeMutablePointer<Void>(self.data),
+                length: self.data.count * sizeof(UInt8), freeWhenDone: false)
+            
+            if raw.writeToFile(path, atomically: true) {
+                
+                completion(path: path, error: nil)
+                
+            } else {
+                
+                completion(path: nil, error: FileError.WriteError)
+            }
+        }
+    }
     
 }
