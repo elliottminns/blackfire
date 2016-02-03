4b1b21d6fc8f5eb1edb14c28439e71f41b8297e7
Elliott Minns <elliott.minns@me.com>
2016-02-03 17:07:49 +0000

    Renamed the package for the fork

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Vapor",
+    name: "Blackfish",
     dependencies: [ 
         //.Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0) //testing
     ]
