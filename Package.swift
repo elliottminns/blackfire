e9d3f57768c483f924ff249a6c7be9469ed63c18
Elliott Minns <elliott.minns@me.com>
2016-03-04 19:08:10 +0000

    Fixed package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Blackfish",
     dependencies: [
-        //.Package(url: "https://github.com/elliottminns/echo.git",
-        .Package(url: "../echo/",
+        .Package(url: "https://github.com/elliottminns/echo.git",
                  majorVersion: 0)
 /*        .Package(url: "https://github.com/elliottminns/vaquita.git",
                  Version(0, 1, 1)),
