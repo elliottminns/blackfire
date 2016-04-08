dbd7f8a83f90f0b82c85baafcb972d4a3317a205
Elliott Minns <elliott.minns@me.com>
2016-04-08 16:53:44 +0100

    Updated

--- Package.swift
+++ Package.swift
@@ -4,10 +4,6 @@ let package = Package(
     name: "Blackfish",
     dependencies: [
         .Package(url: "https://github.com/elliottminns/echo.git",
-                 Version(0, 6, 0))
-/*        .Package(url: "https://github.com/elliottminns/vaquita.git",
-                 Version(0, 1, 1)),
-        .Package(url: "https://github.com/elliottminns/narwhal.git",
-                 Version(0, 1, 0))*/
+                 Version(0, 6, 2))
     ]
 )
