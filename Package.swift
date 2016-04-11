623662c7b4353bc607dfac972e2425b520f05ae9
Elliott Minns <elliott.minns@me.com>
2016-04-11 22:44:01 +0100

    Updated package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "Blackfish",
     dependencies: [
         .Package(url: "https://github.com/elliottminns/echo.git",
-                 Version(0, 6, 2))
+                 Version(0, 6, 3))
     ]
 )
